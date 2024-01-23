16b5ed94c3606bfdb7e8ff98ad90e7e73fed30de
Tanimpathan <38831262+Tanimpathan@users.noreply.github.com>
2024-01-23 11:55:38 +0600

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <div className="fixed top-0 left-1/2 w-full h-[4.15rem] bg-white bg-opacity-80 rounded-none border border-white border-opacity-40 shadow-lg shadow-black/[0.03]"></div>
+    </header>
+  );
+}
